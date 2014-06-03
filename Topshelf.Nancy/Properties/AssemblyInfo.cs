6bfa4729bb9d3cda7209a8d0f41826d553aac9de
Jaimal Chohan <jaimalchohan@gmail.com>
2014-06-03 19:44:16 +0100

    Sort out nusepc and assemblyinfos

--- Topshelf.Nancy/Properties/AssemblyInfo.cs
+++ Topshelf.Nancy/Properties/AssemblyInfo.cs
@@ -1,36 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Topshelf.Nancy")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("JUST EAT PLC")]
 [assembly: AssemblyProduct("Topshelf.Nancy")]
 [assembly: AssemblyCopyright("Copyright © JUST EAT PLC 2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("672ee62d-3d62-46bb-ac37-92d8ab11325b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Configure Number
-//      Revision
-//
-// You can specify all the values or you can default the Configure and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
